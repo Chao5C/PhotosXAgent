8a9fcffeadb68d16569874856bff86c678e3c8e9
Chao5C <148927990+Chao5C@users.noreply.github.com>
2026-09-01 20:31:09 +0800

    Delete fronted directory

--- fronted/src/views/Error/404.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <el-result icon="warning" title="页面不存在" sub-title="请检查地址或返回图库">
-    <template #extra>
-      <el-button type="primary" @click="$router.push('/gallery')">回到图库</el-button>
-    </template>
-  </el-result>
-</template>
